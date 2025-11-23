1ca17aa0b8226df40da540d50caa2565d0db6951
Kristaps Kaupe <kristaps@blogiem.lv>
2025-11-24 00:39:34 +0200

    Revert "automatically update xbps package database and xbps itself daily"
    This reverts commit ee28b63c7e96ec5679931d3a20e8f1ae95c2486a.

--- base/voidlinux.sh
+++ base/voidlinux.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# shellcheck disable=SC2154
-
 # base OS tweaks.
 # the script assumes SYSUPDATES_CHANNEL env var is set to the desired changes
 # channel, whatever the update.sh accept which is typically "dev" or "master".
@@ -33,26 +31,4 @@ if [ ! -f /etc/doas.conf ]; then
 permit nopass root
 permit setenv { -ENV PS1=\$DOAS_PS1 SSH_AUTH_SOCK } :wheel
 EOF
-fi
-
-# automatically update xbps package database and xbps itself daily
-if [ ! -f /etc/cron.daily/xbps-selfupdate ]; then
-    cat <<EOF > /etc/cron.daily/xbps-selfupdate
-#!/bin/sh
-BACKUP_XBPS="/usr/local/sbin/xbps-install.static"
-if [ -z "$XBPS_SELFUPDATE_LOCK" ]; then
-    lockfile=/run/lock/xbps-selfupdate.lock
-    exec env XBPS_SELFUPDATE_LOCK=1 \
-        flock --exclusive --timeout 900 "$lockfile" "$0"
-fi
-set -e
-xbps-install -s
-if xbps-install -y -u xbps; then
-    exit 0
-fi
-if [ -x "$BACKUP_XBPS" ]; then
-    $BACKUP_XBPS -y -u xbps
-fi
-EOF
-    chmod +x /etc/cron.daily/xbps-selfupdate
 fi
